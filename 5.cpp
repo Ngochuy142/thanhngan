c5b1be73381dd149fcc84ead2c3d81a5402275be
thanhNgan13 <89728233+thanhNgan13@users.noreply.github.com>
2021-11-27 21:06:56 +0700

    Add files via upload

---
+++ 5.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+  
+using namespace std;
+ 
+int fibonacci(int &x) {
+    int x0 = 1 ;
+    int x1 = 1 ;
+    if ( (x==1) || (x==2) ) 
+        return 1;
+    int s;
+    for (int i = 3; i <= x; i++) {
+        s = x0 + x1;
+        x0 = x1;
+        x1 = s;
+    }
+    return s;
+}
+ 
+int main() {
+    int x;
+    cout <<"nhap so x \n";
+    cin >> x;
+    if ( (x<1) || (x>30) )
+        cout << "So " << x << " khong nam trong khoanrg [1;30] \n";
+    else 
+        cout << fibonacci(x);
+    return 0;
+    
+}
