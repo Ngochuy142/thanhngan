4430500df37d0139b0edefe80ebb67665da8e39b
thanhNgan13 <89728233+thanhNgan13@users.noreply.github.com>
2022-03-26 09:48:04 +0700

    Update Source1.c

--- Source1.c
+++ Source1.c
@@ -14,16 +14,38 @@ int EndList(List L);
 void Insert_List(lb X, int P, List* L);
 void ReadList(List* LX, List *LY);
 void PrintList(List LX, List LY);
-lb tong(lb* A, int n);
-lb tongBinhPhuong(lb* A, int n);
-lb tich(lb* A, lb* B, int n);
-void tinhLog(lb* A, int n);
+lb summation(lb* A, int n);
+lb sumOfSquares(lb* A, int n);
+lb multiplication(lb* A, lb* B, int n);
+void logarithm(lb* A, int n);
 
 int main() {
+	//printf("_________________________________________ Do An PBL 1: Phuong phap tinh _________________________________________ \n\n");
+	//printf("\t\tTen de tai: 910\n");
+	//printf("\t\tNguoi thuc hien:\n");
+	//printf("\t\t1/ Phan Thanh Ngan \n");
+	//printf("\t\t2/ Nguyen Ngoc Huy\n");
+	//printf("\t\t Cac chuc nang cua chuong trinh:\n");
+	//printf("\t\t 1/ Doc n, cac phan tu cua vecto x, vector y tu file\n");
+	//printf("\t\t 2/ Doc n, cac phan tu cua vecto x, vector y tu ban phim\n");
+	//printf("\t\t 3/ In ket qua he so a, b ra man hinh \n");
+	//printf("\t\t 4/ Luu ket qua he so a, b vao file\n");
+	//printf("\t\t 5/ Ket thuc chuong trinh \n");
 	List LX;
 	List LY;
-	ReadList(&LX, &LY);
-	PrintList(LX, LY);
+	/*while (1) {
+		printf("Chon chuc nang ban muon thuc hien:\n");
+		int n;
+		scanf("%d", &n);
+		switch (n)
+		{
+		case 1:
+		default:
+			break;
+		}*/
+		ReadList(&LX, &LY);
+		PrintList(LX, LY);
+	//}
 	return 0;
 }
 
@@ -43,15 +65,18 @@ void Insert_List(lb X, int P, List* L) {
 	L->Last++;
 }
 void ReadList(List* LX, List *LY) {
+	char s[100];
+	scanf_s("%s", s);
 	lb X, Y;
+	int n;
 	MakeNull_List(LX);
 	MakeNull_List(LY);
 	FILE* myFile = NULL;
-	fopen_s(&myFile, "D:\\def.INP.txt", "r");
+	fopen_s(&myFile, "Text.txt", "r");
 	if (myFile == NULL) {
 		exit(1);
 	}
-	int n = fgetc(myFile) - 48;
+	fscanf(myFile, "%d", &n);
 	lb** A = malloc(2 * sizeof(lb*));
 	for (int i = 0; i < 2; i++) {
 		if (A) {
@@ -92,8 +117,8 @@ void PrintList(List LX, List LY) {
 		printf("%0.3Lf\t", LY.Elements[i]);
 	}
 	printf("\n\n");
-	tinhLog(LX.Elements, n);
-	tinhLog(LY.Elements, n);
+	logarithm(LX.Elements, n);
+	logarithm(LY.Elements, n);
 	printf("LogX |\t");
 	for (int i = 0; i < n; i++) {
 		printf("%0.3Lf\t", LX.Elements[i]);
@@ -104,40 +129,40 @@ void PrintList(List LX, List LY) {
 		printf("%0.3Lf\t", LY.Elements[i]);
 	}
 	printf("\n\n");
-	lb Xi = tong(LX.Elements, n);
-	lb Xi2 = tongBinhPhuong(LX.Elements, n);
-	lb XiYi = tich(LX.Elements, LY.Elements, n);
-	lb Yi = tong(LY.Elements, n);
+	lb Xi = summation(LX.Elements, n);
+	lb Xi2 = sumOfSquares(LX.Elements, n);
+	lb XiYi = multiplication(LX.Elements, LY.Elements, n);
+	lb Yi = summation(LY.Elements, n);
 	lb det = n * Xi2 - Xi * Xi;
 	lb det1 = Xi2 * Yi - Xi * XiYi;
 	lb det2 = XiYi * n - Xi * Yi;
-	/*printf("%0.3Lf\t%0.3Lf\t%0.3Lf\t%0.3Lf\n\n", Xi, Xi2, XiYi, Yi);
-	printf("%0.3Lf\t%0.3Lf\n\n", det1 / det, det2 / det);*/
+	printf("%0.3Lf\t%0.3Lf\t%0.3Lf\t%0.3Lf\n\n", Xi, Xi2, XiYi, Yi);
+	printf("%0.3Lf\t%0.3Lf\n\n", det1 / det, det2 / det);
 
 	printf("a = %0.3Lf\tb = %0.3Lf\n\n", pow(10, (det1 / det)), det2 / det);
 }
-lb tong(lb* A, int n) {
+lb summation(lb* A, int n) {
 	lb s = 0;
 	for (int i = 0; i < n; i++) {
 		s += *(A + i);
 	}
 	return s;
 }
-lb tongBinhPhuong(lb* A, int n) {
+lb sumOfSquares(lb* A, int n) {
 	lb s = 0;
 	for (int i = 0; i < n; i++) {
 		s += pow(*(A + i), 2);
 	}
 	return s;
 }
-lb tich(lb* A, lb* B, int n) {
+lb multiplication(lb* A, lb* B, int n) {
 	lb s = 0;
 	for (int i = 0; i < n; i++) {
 		s += *(A + i) * *(B + i);
 	}
 	return s;
 }
-void tinhLog(lb* A, int n) {
+void logarithm(lb* A, int n) {
 	for (int i = 0; i < n; i++) {
 		*(A + i) = (lb)log10(*(A + i));
 	}
